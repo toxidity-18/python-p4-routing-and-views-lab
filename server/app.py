7ce310844cefeba7c025e868b7837435a035c001
toxidity-18 <desavigatunganga@gmail.com>
2024-10-06 13:30:17 +0300

    first

--- server/app.py
+++ server/app.py
@@ -1,8 +1,34 @@
-#!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_param(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:count>')
+def count(count):
+    return '\n'.join(str(i) for i in range(count)) + '\n'
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        return "Invalid operation", 400
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
